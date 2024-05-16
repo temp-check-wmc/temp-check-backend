7301a2d36c2fb64a34cbb7eaf3d21575edc7ea1b
oliverpk2000 <oliver.pecek@gmail.com>
2024-05-16 08:34:28 +0100

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from sense_hat import SenseHat
+import requests
 
-print("hello world")
+firebase_url = "https://temp-check-4c71c-default-rtdb.europe-west1.firebasedatabase.app"
+
+def put_temp_data(url):
+    req = requests.put("{}/test.json".format(url), data = {'key':'value'})
+    print(req)
+
+put_temp_data(firebase_url)
+
