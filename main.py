f28300ca046704b454cb77a767bb02e519125b7b
oliverpk2000 <oliver.pecek@gmail.com>
2024-05-16 09:34:04 +0100

    working request

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from sense_hat import SenseHat
 import requests
+from datetime import datetime
+import json
 
 firebase_url = "https://temp-check-4c71c-default-rtdb.europe-west1.firebasedatabase.app"
 
-def put_temp_data(url):
-    req = requests.put("{}/test.json".format(url), data = {'key':'value'})
-    print(req)
+def get_temperature():
+    sense = SenseHat()
+    sense.clear()
+    return sense.get_temperature()
 
-put_temp_data(firebase_url)
+
+def put_temperature_data(base_url):
+    timestamp = datetime.now()
+
+    url ="{}/{}/{}/{}/{}/{}.json".format(base_url, timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute)
+
+    temperature_data = {}
+    temperature_data["temperature"] = get_temperature()
+    
+    res = requests.put(url, json=temperature_data)
+    
+    print(res)
+    print(url)
+    print(res.json())
+
+put_temperature_data(firebase_url)
 
