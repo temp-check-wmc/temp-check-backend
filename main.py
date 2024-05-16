00b402582fe22b927c21ea2dc535bed1d1941a7f
oliverpk2000 <oliver.pecek@gmail.com>
2024-05-16 13:17:51 +0100

    added humidity and pressure

--- main.py
+++ main.py
@@ -11,18 +11,40 @@ def get_temperature():
     return sense.get_temperature()
 
 
+def get_humidity():
+    sense = SenseHat()
+    sense.clear()
+    return sense.get_humidity()
+
+
+def get_pressure():
+    sense = SenseHat()
+    sense.clear()
+    return sense.get_pressure()
+
+
 def put_temperature_data(base_url):
     timestamp = datetime.now()
 
-    url ="{}/{}/{}/{}/{}/{}.json".format(base_url, timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute)
+    url ="{}/{}/{}/{}/{}/{}.json".format(base_url,
+                                         timestamp.year, 
+                                         timestamp.month, 
+                                         timestamp.day, 
+                                         timestamp.hour, 
+                                         timestamp.minute)
 
     temperature_data = {}
+
     temperature_data["temperature"] = get_temperature()
-    
+    temperature_data["humidity"] = get_humidity()
+    temperature_data["pressure"] = get_pressure()
+
     res = requests.put(url, json=temperature_data)
     
     print(url)
     print(res.json())
 
-put_temperature_data(firebase_url)
+
+if __name__ == "__main__":
+    put_temperature_data(firebase_url)
 
