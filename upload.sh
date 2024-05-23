6f51ec9a7856d1d8f9a6012a496b8d65d0ebc660
oliver pecek <60604086+oliverpk2000@users.noreply.github.com>
2024-05-23 08:03:19 +0200

    fixed upload date and time not appearing in runtime.err

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "UPLOAD EXECUTION TIME: $(date)" >> runtime.out 2>> runtime.err
+echo "UPLOAD EXECUTION TIME: $(date)" >> runtime.out >> runtime.err
 python3 -u main.py >> runtime.out 2>> runtime.err &
